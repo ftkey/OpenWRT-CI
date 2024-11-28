9e34c572419f5b0a2c56fdecb0d35790e9c72ba1
LaoT <ftkey@qq.com>
2024-11-28 11:54:43 +0800

    Merge branch 'private' of https://github.com/ftkey/OpenWRT-CI into private
    # Conflicts:
    #	99-init_auto_backup.sh

--- 99-init_auto_backup.sh
+++ 99-init_auto_backup.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
-storage_path=$(blkid | grep 'PARTLABEL="storage"' | awk '{print $1}' | sed 's/:$//')
-storage_mount_path=$(mount | grep '/dev/mmcblk0p23' | awk '{print $3}')
-if [ -d $storage_mount_path ]; then
-	echo "
-	backupfilename=\$(date +%Y%m%d_%H%M%S)
-	tar -czvf $backuppath1/openwrt-backup_$backupfilename-etc.tar.gz /etc 
-	opkg list-installed > $backuppath1/openwrt-backup_$backupfilename-soft-list.txt
- 	" > $storage_mount_path/auto_backup.sh
-	chmod +x $storage_mount_path/auto_backup.sh
-	echo "auto_backup自定义设置完成"
-	if [ -f "/etc/crontabs/root" ]; then
-		echo "0 5 * * *  $storage_mount_path/auto_backup.sh" >> /etc/crontabs/root
-		echo "crontabs自定义设置完成"
-	fi
+storage_path=$(blkid | grep -E 'PARTLABEL="storage"|PARTLABEL="primary"' | awk '{print $1}' | sed 's/:$//')
+storage_mount_path=$(mount | grep "$storage_path" | awk '{print $3}')
+if [ -d "$storage_mount_path" ]; then
+    echo '
+    backupdatetime=$(date +%Y%m%d_%H%M%S)
+    tar -czvf /mnt/mmcblk0p23/openwrt-backup_$backupdatetime-etc.tar.gz /etc 
+    opkg list-installed > /mnt/mmcblk0p23/openwrt-backup_$backupdatetime-soft-list.txt
+    '  > "$storage_mount_path/auto_backup.sh"
+    chmod +x "$storage_mount_path/auto_backup.sh"
+    echo "auto_backup自定义设置完成"
+    if [ -f "/etc/crontabs/root" ]; then
+        echo "0 5 * * *  $storage_mount_path/auto_backup.sh" >> /etc/crontabs/root
+        echo "crontabs自定义设置完成"
+    fi
 else
-	echo "auto_backup自定义未初始化"
+    echo "auto_backup自定义未初始化"
 fi
