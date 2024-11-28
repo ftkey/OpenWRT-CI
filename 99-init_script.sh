69bfaccc2e9c9ca4bb9449203ca3a40a4357b0e3
LaoT <ftkey@qq.com>
2024-11-28 11:39:34 +0800

    Update 99-init_script.sh

--- 99-init_script.sh
+++ 99-init_script.sh
@@ -101,23 +101,7 @@ else
 	echo "zerotier自定义未初始化"
 fi
 
-storage_path=$(blkid | grep 'PARTLABEL="storage"' | awk '{print $1}' | sed 's/:$//')
-storage_mount_path=$(mount | grep '/dev/mmcblk0p23' | awk '{print $3}')
-if [ -d $storage_mount_path ]; then
-	echo "
-	backupfilename=\$(date +%Y%m%d_%H%M%S)
-	tar -czvf $backuppath1/openwrt-backup_$backupfilename-etc.tar.gz /etc 
-	opkg list-installed > $backuppath1/openwrt-backup_$backupfilename-soft-list.txt
- 	" > $storage_mount_path/auto_backup.sh
-	chmod +x $storage_mount_path/auto_backup.sh
-	echo "auto_backup自定义设置完成"
-	if [ -f "/etc/crontabs/root" ]; then
-		echo "0 5 * * *  $storage_mount_path/auto_backup.sh" >> /etc/crontabs/root
-		echo "crontabs自定义设置完成"
-	fi
-else
-	echo "auto_backup自定义未初始化"
-fi
+wget -O - https://github.com/ftkey/OpenWRT-CI/raw/refs/heads/private/99-init_auto_backup.sh | bash
 
 
 
