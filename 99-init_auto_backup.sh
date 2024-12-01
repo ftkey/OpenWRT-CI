802b479f4304a7b262fc9f04a532884569b4e38f
ftkey <ftkey@qq.com>
2024-12-01 17:02:10 +0800

    99-init_auto_backup.sh

--- 99-init_auto_backup.sh
+++ 99-init_auto_backup.sh
@@ -1,19 +1,28 @@
 #!/bin/bash
 
-storage_path=$(blkid | grep -E 'PARTLABEL="storage"|PARTLABEL="primary"' | awk '{print $1}' | sed 's/:$//')
-storage_mount_path=$(mount | grep "$storage_path" | awk '{print $3}')
-if [ -d "$storage_mount_path" ]; then
-    echo '
-    backupdatetime=$(date +%Y%m%d_%H%M%S)
-    tar -czvf /mnt/mmcblk0p23/openwrt-backup_$backupdatetime-etc.tar.gz /etc 
-    opkg list-installed > /mnt/mmcblk0p23/openwrt-backup_$backupdatetime-soft-list.txt
-    '  > "$storage_mount_path/auto_backup.sh"
-    chmod +x "$storage_mount_path/auto_backup.sh"
-    echo "auto_backup自定义设置完成"
+storage_dev_path=$(blkid | grep -E 'PARTLABEL="storage"|PARTLABEL="primary"' | awk '{print $1}' | sed 's/:$//')
+storage_path=$(mount | grep "$storage_dev_path" | awk '{print $3}')
+
+if cmd=$(command -v apk) > /dev/null; then
+    flags="list -I"
+else
+    cmd=$(command -v opkg)
+    flags="list-installed"
+fi
+
+if [ -d "$storage_path" ]; then
+    cat << EOF > "$storage_path/auto_backup.sh"
+    #!/bin/bash
+    backupdatetime=\$(date +%Y%m%d_%H%M%S)
+    tar -czvf "$storage_path/openwrt-backup_\$backupdatetime-etc.tar.gz" /etc
+    $cmd $flags > "$storage_path/openwrt-backup_\$backupdatetime-soft-list.txt"
+EOF
+
+    chmod +x "$storage_path/auto_backup.sh"
     if [ -f "/etc/crontabs/root" ]; then
-        echo "0 5 * * *  $storage_mount_path/auto_backup.sh" >> /etc/crontabs/root
+        echo "0 5 * * * $storage_path/auto_backup.sh" >> /etc/crontabs/root
         echo "crontabs自定义设置完成"
     fi
 else
-    echo "auto_backup自定义未初始化"
+    echo "无可读写的挂载分区"
 fi
