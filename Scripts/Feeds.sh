82dee67fc6e6a23d88a28f4d76cea1355c281c1a
Laot <ftkey@qq.com>
2024-12-05 00:32:28 +0800

    Update Feeds.sh
    Signed-off-by: Laot <ftkey@qq.com>

--- Scripts/Feeds.sh
+++ Scripts/Feeds.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #LEDE平台调整
-if [[ $WRT_REPO == *"lede"* ]]; then
-	sed -i '/openwrt-23.05/d' feeds.conf.default
-	sed -i 's/^#\(.*luci\)/\1/' feeds.conf.default
-fi
+#if [[ $WRT_REPO == *"lede"* ]]; then
+#	sed -i '/openwrt-23.05/d' feeds.conf.default
+#	sed -i 's/^#\(.*luci\)/\1/' feeds.conf.default
+#fi
