56dd3b2e21ed1bcbc9c8a9e211e4cf3241d3af5b
Laot <ftkey@qq.com>
2025-01-17 15:08:40 +0800

    可精简备份
    Signed-off-by: Laot <ftkey@qq.com>

--- 99-init_auto_backup.sh
+++ 99-init_auto_backup.sh
@@ -18,6 +18,30 @@ cat << EOF >> "$storage_path/auto_backup.sh"
 backupdatetime=\$(date +%Y%m%d_%H%M%S)
 sysupgrade -b "$storage_path/openwrt-backup_\$backupdatetime.tar.gz"
 \$cmd \$flags > "$storage_path/openwrt-backup_\$backupdatetime-soft-list.txt"
+######
+### 如果存在auto_backup_keep_files.txt，读取精简只保留内容
+#etc/hosts
+#etc/passwd
+#etc/crontabs/
+#etc/config/dhcp
+######
+if [[ -s $storage_path/auto_backup_keep_files.txt ]]; then
+	rm -rf $storage_path/backup_extracted $storage_path/backup_keep
+	mkdir $storage_path/backup_extracted $storage_path/backup_keep
+	
+	tar -xzf "$storage_path/openwrt-backup_\${backupdatetime}.tar.gz" -C $storage_path/backup_extracted
+	while IFS= read -r file; do
+	    # 创建目标目录
+	    mkdir -p "$storage_path/backup_keep/\$(dirname "\$file")"
+	    # 复制文件或目录
+	    cp -r "$storage_path/backup_extracted/\$file" "$storage_path/backup_keep/\$file"
+	done < $storage_path/auto_backup_keep_files.txt
+	tar -czf $storage_path/openwrt-backup_\${backupdatetime}-lite.tar.gz -C $storage_path/backup_keep etc
+	
+	rm -rf $storage_path/backup_extracted $storage_path/backup_keep
+fi
+
+
 EOF
 
     chmod +x "$storage_path/auto_backup.sh"
